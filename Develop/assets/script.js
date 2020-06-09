4b15f5df5cc0749cfc80e75b1cef80b53e2ad8b4
Dominika <dominikaperkowska@gmail.com>
2020-06-09 14:03:00 -0400

    added moment/current day

---
+++ Develop/assets/script.js
@@ -0,0 +1,27 @@
+// DOM Ready
+$(document).ready(function() {
+const submit = document.getElementsByTagName('button');
+
+// Moment
+const now = moment().format('MMMM DD, YYYY');
+// add current day to its html id
+$('#currentDay').text(now);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DOM Ready Close
+});
